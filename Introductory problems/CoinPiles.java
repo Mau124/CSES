083931e92f7eff7721d29e015cbfc313f38a642b
Mau124 <andresperez1024@gmail.com>
2023-11-03 20:41:47 -0600

    Solution for CoinPiles added

---
+++ Introductory problems/CoinPiles.java
@@ -0,0 +1,144 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class CoinPiles {
+	
+	public static void main(String[] args) {
+		FastScanner fs = new FastScanner();
+		PrintWriter out = new PrintWriter(System.out);
+		
+		// 1. Get what is the end DSU after relaesing hands
+		// 2. Do the same we did in the cut graphs problems
+		int n = fs.nextInt();
+		for(int nn=0; nn<n; ++nn) {
+			int p1 = fs.nextInt(), p2= fs.nextInt();
+			if((2*p2 - p1)%3==0 && (2*p1 - p2)%3==0 && (2*p1-p2)>=0 && (2*p2-p1)>=0) {
+				System.out.println("YES");
+			} else {
+				System.out.println("NO");
+			}
+		}
+
+		out.close();
+	}
+	
+	static class DSU {
+		int size;
+		int[] p, r, sum, min, max, experience;
+		
+		DSU(int size) {
+			this.size = size;
+			p = new int[size];
+			r = new int[size];
+			sum = new int[size];
+			min = new int[size];
+			max = new int[size];
+			experience = new int[size];
+			for(int i=0; i<size; ++i) {
+				p[i] = i;
+				r[i] = 1;
+				sum[i] = 1;
+				min[i] = i;
+				max[i] = i;
+				experience[i] = 0;
+			}
+		}
+		
+		// Returns if nodeA and nodeB belongs to the same disjoint set
+		boolean find(int nodeA, int nodeB) {
+			return (get(nodeA) == get(nodeB));
+		}
+		
+		// O(alpha(m, n)) -> Ackermann function, almost O(1)
+		// Path compression
+		int get(int node) {
+			if(p[node] != node)
+				p[node] = get(p[node]);
+			return p[node];	
+		}
+		
+		// O(alpha(m, n)) -> Ackermann function, almost O(1)
+		// Rank heuristic to union the sets
+		// Always join smaller to larger
+		void union(int nodeA, int nodeB) {
+			nodeA = get(nodeA);
+			nodeB = get(nodeB);
+			
+			if(nodeA == nodeB) 
+				return;
+			
+			if(r[nodeA] == r[nodeB]) {
+				r[nodeA]++;
+			}
+			if(r[nodeA] > r[nodeB]) {
+				p[nodeB] = p[nodeA];
+				sum[nodeA] += sum[nodeB];
+				min[nodeA] = Math.min(min[nodeA], min[nodeB]);
+				max[nodeA] = Math.max(max[nodeA], max[nodeB]);
+			} else {
+				p[nodeA] = nodeB;
+				sum[nodeB] += sum[nodeA];
+				min[nodeB] = Math.min(min[nodeA], min[nodeB]);
+				max[nodeB] = Math.max(max[nodeA], max[nodeB]);
+			}
+		}
+		
+		// Returns the minimum value in the set where nodeA is
+		int getMin(int nodeA) {
+			nodeA = get(nodeA);
+			return min[nodeA];
+		}
+		
+		// Returns the maximum value in the set where nodeA is
+		int getMax(int nodeA) {
+			nodeA = get(nodeA);
+			return max[nodeA];
+		}
+		
+		// Return the number of elements in the set where nodeA is
+		int getElements(int nodeA) {
+			nodeA = get(nodeA);
+			return sum[nodeA];
+		}
+		
+		// Add experience to each player on group
+		void add(int nodeA, int exp) {
+			nodeA = get(nodeA);
+			experience[nodeA] += exp;
+		}
+		
+		// Return the experience of player p
+		int getExperience(int nodeA) {
+			nodeA = get(nodeA);
+			return experience[nodeA];
+		}
+	}
+	
+	static class FastScanner {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer("");
+		
+		public String next() {
+			while(!st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			
+			return st.nextToken();
+		}
+		
+		public int nextInt() {
+			return Integer.parseInt(next());
+		}
+		
+		public long nextLong() {
+			return Long.parseLong(next());
+		}
+	}
+}
